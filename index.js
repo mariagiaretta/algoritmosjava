7e4fde24cec2c30e9338eef9b06323f626008e0c
Maria Eduarda Giaretta <mariihgiaretta@gmail.com>
2024-04-01 10:25:55 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>JavaScript Hello World!</title>
+
+    <script>
+         function main() {
+             alert("Hello World!"):
+        }
+    </script>
+</head>
+<body>
+
+</body>
+</html>
